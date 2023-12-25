929de9c73d9672bb6012d978a67d65e51e6ae144
Mick West <mick@mickwest.com>
2023-12-24 23:52:12 -0800

    Fixed camera start position for westjet

--- src/sitch/SitWestJet.js
+++ src/sitch/SitWestJet.js
@@ -13,8 +13,9 @@ export const SitWestJet = {
     videoFile: "../sitrec-videos/private/UAP Sighting by WestJet Passengers 12-18-23 16-05 clip.mp4",
     startTime: "2023-12-19T03:56:12.560Z",
 
-
-
     ptz: {az: -79.6, el: 3.7, fov: 25.7, showGUI: true},
+
+    startCameraPositionLLA:[38.602145,-86.506588,4159762.165337],
+    startCameraTargetLLA:[38.603456,-86.509621,4158895.037381],
 
 }
