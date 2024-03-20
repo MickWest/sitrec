e29e286b79d4c85d895ac09c5a3c918ef59f8684
Mick West <mick@mickwest.com>
2024-03-20 00:07:54 -0700

    Cleanup and commenting

--- src/sitch/SitChilean.js
+++ src/sitch/SitChilean.js
@@ -4,15 +4,10 @@ export const SitChilean = {
     menuName: "Chilean Navy IB 6830",
     isTextable: true,
 
-    planeCameraFOV: 0.75    ,
-    targetSize: 1, // in feet
-
-    tilt: 0,
-
     frames: 17969,
     terrain: {lat: -33.2611, lon: -71.192388, zoom: 9, nTiles: 8},
 
-   // note files are automatically loaded into a data structure
+    // note files are automatically loaded into a data structure
     // that varies based on the extension
     // e.g. a .csv file will be loaded by FileMan.loadCSV
     files: {
@@ -20,43 +15,63 @@ export const SitChilean = {
         KMLTarget: "chilean/IB6830 - Incorporating Radar Positions.kml",
 //        TargetObjectFile: 'models/A340-600-F18Engine.glb',
         TargetObjectFile: 'models/A340-600.glb',
-        DataFile: 'chilean/Chilean Navy Extracted Data 720.csv',
+        DataFile: 'chilean/Chilean Navy Extracted Data 720.csv', // 720p version as videos have slight differences
     },
     videoFile: "../sitrec-videos/public/Chilean Navy 13-51-55 from HD 720p.mp4",
     startTime: "2014-11-11T16:51:55Z",
 
     mainCamera: {
         fov:30,
-        startCameraPosition: [-126967.77, 61278.38, 196946.50],
-        startCameraTarget: [-126503.73, 61040.85, 196093.13],
+        startCameraPositionLLA:[-34.781951,-73.204317,80696.379925],
+        startCameraTargetLLA:[-34.776516,-73.196683,80342.826209],
     },
     cameraTrack: {},
 
+    // single sold color of the sky.
     skyColor: 'skyblue',
 
-    lookView: {             left: 0.5, top: 0.5, width: -1.77777, height: 0.5,
+    // The three views are the main view, the look view, and the video view
+    // The look view is the view from the camera to the target
+    // note it has an effect of FLIRShader to make it look like an IR camera
+    lookView: { left:0.5, top:0.5, width: -1.77777, height: 0.5,
         effects: {FLIRShader: {},},},
 
-    videoView: {             left: 0.5, top: 0, width: -1.77777, height: 0.5,},
-    mainView:{left:0.0, top:0, width:0.625,height:1},
+    // The video view is the view of the original video
+    videoView:{ left:0.5, top:0,   width:-1.77777, height: 0.5,},
 
+    // The main view is the view of the 3D scene where the user can move the camera around
+    mainView: { left:0.0, top:0,   width:0.625,    height: 1},
+
+    // setup of common tracks, cameras, labels, etc
     include_KMLTrackToTrack: true,
 
     // // Wind is needed to adjust the target planes heading relative to motion in the TailAngleGraph and for the model angle
     targetWind: {from: 270, knots: 0, name: "Target", arrowColor: "cyan"},
 
+    // the target object is the model of the plane, as specified by the file
+    // tiltType of "banking" will cause the model to bank in turns
+    // wind is used to adjust the model's orientation (i.e it has to head more into the wind)
     targetObject:{file: "TargetObjectFile", tiltType: "banking", wind: "targetWind",},
 
+    // wesCamFOV replicates the field of view of the camera that took the video
+    // This is specific to this case in that there's adjustments for the "mode" (IR or EOW/EON)
+    // but the generic calculation takes a reference focal length and the fov at that focal length
+    // then calculates vFOV based on that.
     wescamFOV: {file: "DataFile", focalIndex: 2, modeIndex: 1, len: 675, fov: 0.915},
 
+    // Smooth the target track by the default amount
     smoothTrack: {track: "targetTrack"},
 
+    // Adds a graph of the tail angle of the target plane
+    // this is relative to the line of sight from the camera to the target
     tailAngleGraph: {targetTrack: "targetTrack", cameraTrack: "cameraTrack", wind: "targetWind",
                      left: 0.0, top: 0, width: .15, height: .25,},
 
+    // Adds a graph of the distance from the camera to the target
     targetDistanceGraph: {targetTrack: "targetTrack", cameraTrack: "cameraTrack",
                           left: 0.0, top: 0.25, width: .15, height: .33,},
 
+    // Adds a sphere of selectable size at the target's position
     targetSizedSphere: { track: "targetTrack", size: 3, color : 0x000000,},
 
 }
