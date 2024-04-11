3489ce516713dad169f89fbcdc7b641bfeb0ce2c
Mick West <mick@mickwest.com>
2024-04-11 05:12:54 -0700

    Fixed bug with line colors not changing

--- src/MatLines.js
+++ src/MatLines.js
@@ -1,10 +1,23 @@
 // Factory for line materials, which need to be updated on resize.
 import {LineMaterial} from "../three.js/examples/jsm/lines/LineMaterial";
+import {Color} from "three";
 
 var matLines = {} // collection of line materials that need updating on resize
 // we make one entry per unique material
 function makeMatLine(color, linewidth = 2, dashed = false) {
-    var key = String(color) + String(linewidth) + String(dashed)
+
+    // if it's not a color object, then make it one
+    if (!color.isColor) {
+        color = new Color(color);
+    }
+
+    // we need a unique ID for the material
+    // so we can update the resolution
+    // we use the color, linewidth, and dashed as the key
+    // first we make a string of the values
+    // color is THREE.Color, convert it to a hex string
+    const hex = color.getHexString()
+    var key = hex + String(linewidth) + String(dashed)
     if (!matLines[key]) {
         var lineMaterial = new LineMaterial({
             color: color,
