b14e6e0433523dee09af49925f2e09c574dbc2f7
Mick West <mick@mickwest.com>
2025-01-02 16:46:19 -0800

    Display the wind heading (from) in the compass

--- src/nodes/CNodeCompassUI.js
+++ src/nodes/CNodeCompassUI.js
@@ -6,6 +6,7 @@ import {getLocalNorthVector, getLocalUpVector} from "../SphericalMath";
 import {Vector3} from "three";
 import {MV3} from "../threeUtils";
 import {ViewMan} from "./CNodeView";
+import {NodeMan} from "../Globals";
 
 export class   CNodeCompassUI extends CNodeViewUI {
 
@@ -49,15 +50,118 @@ export class   CNodeCompassUI extends CNodeViewUI {
 
         // get the three.js camera from the camera node
         const camera = this.in.camera.camera;
-
-        // get local up vector, the headings are the angle about this axis.
-        const up = getLocalUpVector(camera.position);
-
         // get the camera's forward vector, the negative z basis from its matrix
         const forward = MV3(camera.matrixWorld.elements.slice(8,11));
 
+
+        // get the heading of the camera, in radians
+        const heading = this.getCompassHeading(camera.position, forward);
+
+        // convert to 0..360 degrees for display
+        const headingDeg = heading * 180 / Math.PI;
+        // make sure it's positive
+        const headingPos = (headingDeg + 360) % 360;
+        // round to the nearest 0.1 degree
+        const headingRound = Math.round(headingPos * 10) / 10;
+
+        // set the text to the rounded heading
+        this.text.text = headingRound + "°";
+
+        // after updating the text, render the text
+        super.renderCanvas(frame)
+
+        // now draw a centered arrow rotated by the heading
+
+        // make a 2D point at 50,0 (north)
+        // rotate it around 50,50 by the heading
+        // draw a line from 50,50 to the rotated point
+
+
+        const c = this.ctx;
+
+
+        // draw the letter N in the center
+        c.fillStyle = '#FFFFFF';
+        c.font = this.px(17)+'px Arial';
+        c.textAlign = 'center';
+        c.fillText('N', this.px(this.cx), this.py(this.cy+7));
+
+
+        let length = 35;
+
+
+        const targetWind = NodeMan.get("targetWind", false);
+        if (targetWind) {
+            const fromDegrees = targetWind.from;
+            const fromRadians = heading + 2*Math.PI - fromDegrees * Math.PI / 180;
+
+            const c = this.ctx;
+            c.strokeStyle = '#FFFF40';
+            c.lineWidth = 2.5;
+            c.beginPath();
+            const gap = 10;
+            const segment = (length ) / 2
+            // rLine draws lines rotated about cx,cy
+
+            this.rLine(this.cx-3,this.cy-length,this.cx,this.cy-length*0.5,fromRadians);
+            this.rLine(this.cx+3,this.cy-length,this.cx,this.cy-length*0.5,fromRadians);
+            this.rLine(this.cx+3,this.cy-length,this.cx-3,this.cy-length,fromRadians);
+            c.stroke();
+
+        }
+
+        const localWind = NodeMan.get("localWind", false);
+        if (localWind) {
+            const fromDegrees = localWind.from;
+            const fromRadians = heading + 2*Math.PI - fromDegrees * Math.PI / 180;
+
+            const c = this.ctx;
+            c.strokeStyle = '#40FF40';
+            c.lineWidth = 2.5;
+            c.beginPath();
+            const gap = 10;
+            const segment = (length ) / 2
+            // rLine draws lines rotated about cx,cy
+
+            this.rLine(this.cx-3,this.cy-length,this.cx,this.cy-length*0.5,fromRadians);
+            this.rLine(this.cx+3,this.cy-length,this.cx,this.cy-length*0.5,fromRadians);
+            this.rLine(this.cx+3,this.cy-length,this.cx-3,this.cy-length,fromRadians);
+            c.stroke();
+
+        }
+
+
+        // finally the compass line (so it's on top of the wind markers)
+        c.strokeStyle = '#FFFFFF';
+        c.lineWidth = 2.5;
+        c.beginPath();
+        length = 30;
+        const gap = 10;
+        const segment = (length ) / 2
+        // rLine draws lines rotated about cx,cy
+        //this.rLine(this.cx,this.cy+length,this.cx,this.cy-length,heading);
+
+        this.rLine(this.cx,this.cy+length,this.cx,this.cy+gap,heading);
+        this.rLine(this.cx,this.cy-length,this.cx,this.cy-gap,heading);
+
+
+        this.rLine(this.cx,this.cy-length,this.cx-3,this.cy-length*0.5,heading);
+        this.rLine(this.cx,this.cy-length,this.cx+3,this.cy-length*0.5,heading);
+        c.stroke();
+
+
+
+    }
+
+
+    // given position and a vector, return the heading in degrees relative to the north vector at that position
+    getCompassHeading(position, forward) {
+
+        // get local up vector, the headings are the angle about this axis.
+        const up = getLocalUpVector(position);
+
         // get the north vector
-        const north = getLocalNorthVector(camera.position);
+        const north = getLocalNorthVector(position);
 
         // project the forward vector onto the horizontal plane defined by up
         const forwardH = forward.clone().sub(up.clone().multiplyScalar(forward.dot(up)));
@@ -78,55 +182,10 @@ export class   CNodeCompassUI extends CNodeViewUI {
             heading = -heading;
         }
 
-
-
-
-
-        // convert to degrees
-        const headingDeg = heading * 180 / Math.PI;
-        // make sure it's positive
-        const headingPos = (headingDeg + 360) % 360;
-        // round to the nearest 0.1 degree
-        const headingRound = Math.round(headingPos * 10) / 10;
-        // set the text to the rounded heading
-        this.text.text = headingRound + "°";
-
-        // after updating the text, render the text
-        super.renderCanvas(frame)
-
-        // now draw a centered arrow rotated by the heading
-
-        // make a 2D point at 50,0 (north)
-        // rotate it around 50,50 by the heading
-        // draw a line from 50,50 to the rotated point
-
-
-        const c = this.ctx;
-        c.strokeStyle = '#FFFFFF';
-        c.lineWidth = 2.5;
-        c.beginPath();
-        const length = 30;
-        const gap = 10;
-        const segment = (length ) / 2
-        // rLine draws lines rotated about cx,cy
-        //this.rLine(this.cx,this.cy+length,this.cx,this.cy-length,heading);
-
-        this.rLine(this.cx,this.cy+length,this.cx,this.cy+gap,heading);
-        this.rLine(this.cx,this.cy-length,this.cx,this.cy-gap,heading);
-
-
-        this.rLine(this.cx,this.cy-length,this.cx-3,this.cy-length*0.5,heading);
-        this.rLine(this.cx,this.cy-length,this.cx+3,this.cy-length*0.5,heading);
-        c.stroke();
-
-        // draw the letter N in the center
-        c.fillStyle = '#FFFFFF';
-        c.font = this.px(17)+'px Arial';
-        c.textAlign = 'center';
-        c.fillText('N', this.px(this.cx), this.py(this.cy+7));
-
+        return heading;
 
     }
+
 
 
 }
