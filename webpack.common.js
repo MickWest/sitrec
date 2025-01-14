dc0a387daafa590c82cab525772b1318e2d6def6
Mick West <mick@mickwest.com>
2025-01-14 05:42:15 -0800

    Circular dependency check always on in build system

--- webpack.common.js
+++ webpack.common.js
@@ -136,8 +136,7 @@ module.exports = {
             },
             // `onDetected` is called for each module that is cyclical
             onDetected({ module: webpackModuleRecord, paths, compilation }) {
-
-                 return;
+                //return;
 
                 const ignoreModules = ["mathjs"];
                 // return if any of the ignoreModules is a substring of any of the paths
