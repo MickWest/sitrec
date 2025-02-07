37be32e0f0d48a87653687909f7620902ad3984a
Mick West <mick@mickwest.com>
2025-02-07 00:00:30 -0800

    Skip stream_context_create if using curl

--- sitrecServer/cachemaps.php
+++ sitrecServer/cachemaps.php
@@ -143,17 +143,6 @@ if (file_exists($cachedFile)) {
             $extra = $token;
     }
 
-    $options = array(
-        'http'=>array(
-            'method'=>"GET",
-            'header'=>"Accept-language: en\r\n" .
-                "Cookie: foo=bar\r\n" .  // check function.stream-context-create on php.net
-                "User-Agent: Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.102011-10-16 20:23:10\r\n" // i.e. An iPad
-        )
-    );
-    $context = stream_context_create($options);
-
-
     // if we have curl, then use it
     if (function_exists('curl_init')) {
         echo "<br>Using curl";
@@ -168,6 +157,17 @@ if (file_exists($cachedFile)) {
     } else {
         // no curl, so use file_get_contents
         // reportedly less reliable
+
+        $options = array(
+            'http'=>array(
+                'method'=>"GET",
+                'header'=>"Accept-language: en\r\n" .
+                    "Cookie: foo=bar\r\n" .  // check function.stream-context-create on php.net
+                    "User-Agent: Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.102011-10-16 20:23:10\r\n" // i.e. An iPad
+            )
+        );
+        $context = stream_context_create($options);
+
         echo "<br>Using file_get_contents";
         $dataBlob = file_get_contents($url . $extra, false, $context);
     }
