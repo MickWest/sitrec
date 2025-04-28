f48669186e172de11101021891baa0f3a86cf4e8
Mick West <mick@mickwest.com>
2025-04-28 10:33:52 -0700

    Refactored to extract object independent expression evaluation

--- src/nodes/CNodeMath.js
+++ src/nodes/CNodeMath.js
@@ -37,7 +37,7 @@ export class CNodeMath extends CNode {
         // find any node variable and add them to the inputs
         // this ensures that the node is recalculated when the input nodes change
         // and hence other nodes that depend on this node will also be recalculated
-        let matches = this.getNodeVariables(this.math)
+        let matches = getNodeVariables(this.math)
         if (matches) {
             for (let match of matches) {
                 let id = match.slice(1); // remove the leading $
@@ -52,62 +52,57 @@ export class CNodeMath extends CNode {
 
 
 
-    getNodeVariables(expression) {
-        let re = /\$\w+/g
-        return expression.match(re)
-    }
 
     getValueFrame(f) {
         if (!this.math) {
             // empty string is undefined
             return undefined;
         }
-        let expression = this.math;
-        let matches = this.getNodeVariables(this.math)
-        if (matches) {
-            for (let match of matches) {
-                let id = match.slice(1)
-                assert(NodeMan.exists(id), "CNodeMath: node does not exist, id: " + id)
-                let node = NodeMan.get(id);
-                // and replace it in the string with the value of the node at frame f
-                let value = node.getValueFrame(f)
-                // if it's not a number, need more parsing
-                if (typeof value !== "number") {
-                    if (value.position !== undefined) {
-                        // adding a vector value as [x,y,z]
-                        value = '[' + value.position.x + ',' + value.position.y + ',' + value.position.z + ']'
-                    }
-                }
-                expression = expression.replace(match, value)
-            }
-        }
-       // console.log("expression after : ", expression)
-
-        //
-        // // get the input values and add them to a structure that math.js can use
-        // let scope = {}
-        // for (let i in this.inputs) {
-        //     scope[i] = this.inputs[i].getValueFrame(f)
-        // }
-        // // evaluate the math expression
-        // return math.evaluate(expression, scope)
-
-        let result;
-        const context = math.parser();
-        const lines = expression.split(";")
-        for (let line of lines) {
-
-
-            // if line is empty, or just white space, skip it
-            if (line.trim() === "") continue;
-            result = (context.evaluate(line))
-//            console.log("result : ", result)
-        }
-        // if it returns an array of results, we only use the first one
-        if (typeof result === "object") {
-            result = result.entries[0];
-        }
-        return result;
+        return evaluateExpression(this.math, f);
     }
 }
+
+function getNodeVariables(expression) {
+    let re = /\$\w+/g
+    return expression.match(re)
+}
+
+
+export function evaluateExpression(expression, f=0) {
+    let matches = getNodeVariables(expression)
+    if (matches) {
+        for (let match of matches) {
+            let id = match.slice(1)
+            assert(NodeMan.exists(id), "CNodeMath: node does not exist, id: " + id)
+            let node = NodeMan.get(id);
+            // and replace it in the string with the value of the node at frame f
+            let value = node.getValueFrame(f)
+            // if it's not a number, need more parsing
+            if (typeof value !== "number") {
+                if (value.position !== undefined) {
+                    // adding a vector value as [x,y,z]
+                    value = '[' + value.position.x + ',' + value.position.y + ',' + value.position.z + ']'
+                }
+            }
+            expression = expression.replace(match, value)
+        }
+    }
+
+    let result;
+    const context = math.parser();
+    const lines = expression.split(";")
+    for (let line of lines) {
+        // if line is empty, or just white space, skip it
+        if (line.trim() === "") continue;
+        result = (context.evaluate(line))
+//            console.log("result : ", result)
+    }
+    // if it returns an array of results, we only use the first one
+    if (typeof result === "object") {
+        result = result.entries[0];
+    }
+    return result;
+
+}
+
 
